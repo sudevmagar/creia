4663a790256cfc0a95050d122403c323d737077b
sudevmagar <sudebm789@gmail.com>
2025-11-17 13:47:03 +0545

    feat(layout): add LayoutWrapper to wrap client components and hide navbar on auth routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Navbar from "@/components/ui/navbar";
+import LayoutWrapper from "@/components/LayoutWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,8 +35,7 @@ export default function RootLayout({
           enableSystem
           enableColorScheme
         >
-          <Navbar />
-          <main>{children}</main>
+          <LayoutWrapper>{children}</LayoutWrapper>
         </ThemeProvider>
       </body>
     </html>
