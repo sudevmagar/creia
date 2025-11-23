485f4b07117ff3dc16431a4ddbad9ebc3aec6e1c
sudevmagar <sudebm789@gmail.com>
2025-11-23 12:59:29 +0545

    feat(ui): add login/signup navigation links to navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import ThemeToggle from "../ThemeToggle";
 
 export default function Navbar() {
@@ -9,7 +10,18 @@ export default function Navbar() {
         <span className="text-3xl font-bold ">Creia</span>
         <span className="text-3xl font-bold leading-none text-primary">.</span>
       </div>
-      <ThemeToggle />
+      <div>
+        <Link href="/login" className="mr-4 hover:underline">
+          Login
+        </Link>
+        <Link
+          href="/signup"
+          className="mr-4 bg-primary px-4 py-2 rounded-md text-white hover:bg-primary/90 transition-colors"
+        >
+          Get Creia Free
+        </Link>
+        <ThemeToggle />
+      </div>
     </nav>
   );
 }
